35f6d91582cd1e29eef0e1711899d62c45e3c1c0
DIONE  Ousmane Vincent <v.dione@PC437.domaine.local>
2024-06-21 16:25:18 +0200

    Initial

--- src/main/java/org/ovd/MyEntity.java
+++ src/main/java/org/ovd/MyEntity.java
@@ -2,28 +2,16 @@ package org.ovd;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
 
-/**
- * Example JPA entity defined as a Panache Entity.
- * An ID field of Long type is provided, if you want to define your own ID field extends <code>PanacheEntityBase</code> instead.
- *
- * This uses the active record pattern, you can also use the repository pattern instead:
- * .
- *
- * Usage (more example on the documentation)
- *
- * {@code
- *     public void doSomething() {
- *         MyEntity entity1 = new MyEntity();
- *         entity1.field = "field-1";
- *         entity1.persist();
- *
- *         List<MyEntity> entities = MyEntity.listAll();
- *     }
- * }
- */
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class MyEntity extends PanacheEntity {
+
     public String field;
 }
