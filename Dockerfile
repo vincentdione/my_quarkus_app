a1fbf1b5f1e272626c46eea5a7423fbe3c91b803
dioneousmanevincent <dioneousmanevincent@gmail.com>
2024-07-28 12:52:08 +0200

    Adding sonar.properties file

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,54 @@
-# Stage 1: Build the application
-FROM maven:3.8.5-openjdk-17 AS build
+# Use the official JDK 19 image as the base image for the build stage
+FROM openjdk:17-jdk AS build
 
-# Define build arguments
-ARG QUARKUS_PROFILE
-ARG APP_NAME
-# Set the working directory inside the container
-WORKDIR /workspace
+# Enable preview features
+ENV JAVA_OPTS="--enable-preview"
+
+# Set the working directory
+WORKDIR /app
+
+#RUN mvn clean verify sonar:sonar \
+#  -Dsonar.projectKey=my_quarkus_app \
+#  -Dsonar.host.url=http://sonarqube:9000 \
+#  -Dsonar.login=your-sonar-token
+
+# Copy the Maven wrapper
+COPY mvnw ./
+
+COPY .mvn .mvn/
+
+# Copy the pom.xml file
+COPY pom.xml ./
+COPY sonar.properties ./
+
+# Download dependencies and cache them
+RUN ./mvnw dependency:go-offline -B
+
+# Copy the source code
+COPY ./src ./src
+
+# Compile and package the application
+RUN ./mvnw package -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -B -V
 
 
-# Display the working directory and its contents
-RUN echo "Working directory:" && ls -la && pwd
+# Use the official JDK 19 image as the base image for the runtime stage
+FROM openjdk:17-jdk AS runtime
 
-# Display the build arguments
-RUN echo "QUARKUS_PROFILE is set to: $QUARKUS_PROFILE"
-RUN echo "APP_NAME is set to: $APP_NAME"
+# Enable preview features
+ENV JAVA_OPTS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager --enable-preview"
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the build artifacts from the build stage
+#COPY --from=build /app/target/quarkus-app/quarkus-run.jar /app/app.jar
+COPY --from=build /app/target/quarkus-app/lib/ /app/lib/
+COPY --from=build /app/target/quarkus-app/*.jar /app/
+COPY --from=build /app/target/quarkus-app/app/ /app/app/
+COPY --from=build /app/target/quarkus-app/quarkus/ /app/quarkus/
+
+# Set the entrypoint and command to run the application
+ENTRYPOINT ["sh", "-c", "java $JAVA_OPTS -jar /app/quarkus-run.jar"]
+
+
+
--- ci/stack-local/launch.sh
+++ ci/stack-local/launch.sh
@@ -7,24 +7,24 @@
 # Variables
 SONAR_PROJECT_KEY="my_quarkus_app"
 SONAR_PROJECT_NAME="My Quarkus App"
-SONAR_SOURCES="../src/main"
-SONAR_HOST_URL="http://localhost:9000"
+SONAR_SOURCES="src/main/java"
+SONAR_TARGET="target/classes"
+SONAR_HOST_URL="http://sonarqube:9000"
 SONAR_ADMIN_LOGIN="admin"
 SONAR_ADMIN_PASSWORD="admin"
 SONAR_USER="admin"
-SONAR_USER_PASSWORD="passer"
-SONAR_TOKEN_NAME=""
-NETWORK="sonar-network"
+SONAR_USER_PASSWORD="sonar"
+SONAR_TOKEN_NAME="squ_d6575dca0739fb958bbb431e3525c7be63b97205"
+NETWORK="stack-local_sonar-network"
 
 
 
 # Step 2: Start Docker Compose services
 echo "Starting Docker Compose services..."
-docker-compose up -d
+docker compose up -d
 
 # Step 1: Start SonarQube container
-echo "Starting SonarQube container..."
-docker run -d --name sonarqube -p 9000:9000 sonarqube:latest
+#docker run -d --name sonarqube -p 9000:9000 sonarqube:latest
 
 # Function to check if SonarQube is up
 function wait_for_sonarqube() {
@@ -37,25 +37,27 @@ function wait_for_sonarqube() {
 }
 
 # Call the function to wait for SonarQube
-wait_for_sonarqube
+#wait_for_sonarqube
 
 # Additional wait to ensure SonarQube is fully operational
 echo "Giving SonarQube additional time to be fully operational..."
 sleep 60
 
 # Step 2: Create a user and generate a token
-echo "Creating a user and generating a token..."
+#echo "Creating a user and generating a token..."
 
 # Login as admin and create a new user
-docker exec sonarqube curl -u $SONAR_ADMIN_LOGIN:$SONAR_ADMIN_PASSWORD -X POST -d "login=$SONAR_USER&name=$SONAR_USER&password=$SONAR_USER_PASSWORD" "$SONAR_HOST_URL/api/users/create"
+#docker exec sonarqube curl -u $SONAR_ADMIN_LOGIN:$SONAR_ADMIN_PASSWORD -X POST -d "login=$SONAR_USER&name=$SONAR_USER&password=$SONAR_USER_PASSWORD" "$SONAR_HOST_URL/api/users/create"
 
 # Generate a token for the new user
-SONAR_USER_TOKEN=$(docker exec sonarqube curl -u $SONAR_ADMIN_LOGIN:$SONAR_ADMIN_PASSWORD -X POST -d "name=$SONAR_TOKEN_NAME" "$SONAR_HOST_URL/api/user_tokens/generate" | grep -oP '(?<="token":")[^"]*')
+#SONAR_USER_TOKEN=$(docker exec sonarqube curl -u $SONAR_ADMIN_LOGIN:$SONAR_ADMIN_PASSWORD -X POST -d "name=$SONAR_TOKEN_NAME" "$SONAR_HOST_URL/api/user_tokens/generate" | grep -oP '(?<="token":")[^"]*')
 
-echo "Generated token: $SONAR_USER_TOKEN"
+#echo "Generated token: $SONAR_USER_TOKEN"
 
 # Step 3: Run SonarQube analysis
 echo "Running SonarQube analysis..."
+echo ls -l 
+echo pwd
 docker run --rm \
   --network $NETWORK  \
   -v $(pwd):/usr/src/app \
@@ -65,17 +67,9 @@ docker run --rm \
     -Dsonar.projectKey=$SONAR_PROJECT_KEY \
     -Dsonar.projectName="$SONAR_PROJECT_NAME" \
     -Dsonar.sources=$SONAR_SOURCES \
-    -Dsonar.host.url=$SONAR_HOST_URL \
-    -Dsonar.java.binaries=target/classes
+    -Dsonar.host.url=$SONAR_HOST_URL
+    -Dsonar.java.binaries=$SONAR_TARGET
     -Dsonar.login=$SONAR_USER_TOKEN
 
-docker run --rm \
-  --network stack-local_default  \
-  -v $(pwd):/usr/src/app \
-  -w /usr/src/app \
-  sonarsource/sonar-scanner-cli \
-  -Dsonar.projectKey=my_quarkus_app \
-  -Dsonar.sources=src/main  -Dsonar.java.binaries=target/classes \
-  -Dsonar.host.url=http://sonarqube:9000 \
-  -Dsonar.token=squ_d6575dca0739fb958bbb431e3525c7be63b97205
+
 
